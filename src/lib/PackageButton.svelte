e8347e0bec1a9817ee0f0442204e1c71477bb64a
shard77 <sh7user@gmail.com>
2023-09-27 20:51:22 +0200

    added custom component for packages

---
+++ src/lib/PackageButton.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import checkIconBlackBg from "../assets/icons/check-bg-black.svg";
+    import checkIconYellowBg from "../assets/icons/check-bg-yellow.svg";
+    import dashIconBlackBg from "../assets/icons/dash-bg-black.svg";
+    import dashIconYellowBg from "../assets/icons/dash-bg-yellow.svg";
+    import arrowDownIcon from "../assets/icons/arrow-down-black.svg";
+
+    export let label: string;
+    export let options: { label: string; value: string; selected?: boolean }[];
+    export let expanded: boolean = false;
+    let allSelected: boolean = false;
+    let isHovered: boolean = false;
+    let isFocused: boolean = false;
+
+    function toggleOption(index: number) {
+        options[index].selected = !options[index].selected;
+        options = [...options];
+    }
+
+    $: allSelected = options.every(option => option.selected);
+</script>
+
+<button 
+    class="flex flex-row items-center gap-2 w-full rounded-full px-2 py-2 text-left text-sm font-medium 
+        {(isHovered || isFocused) ? 'bg-secondary-container text-black' : 'text-white'}"
+    on:mouseover={() => isHovered = true}
+    on:mouseout={() => isHovered = false}
+    on:focus={() => isFocused = true}
+    on:blur={() => isFocused = false}
+    on:click={() => expanded = !expanded}
+>
+    <img 
+        src={
+            allSelected 
+                ? (isHovered || isFocused ? checkIconBlackBg : checkIconYellowBg )
+                : (isHovered || isFocused ? dashIconBlackBg : dashIconYellowBg )
+        } 
+        alt="status" class="w-7" 
+    >
+    <span class="font-medium text-sm">{label}</span>
+    {#if isHovered || isFocused}
+        <img src={arrowDownIcon} alt="arrow down" class="w-7 ml-auto {expanded ? 'rotate-180 transform' : ''}">
+    {/if}
+</button>
+
+{#if expanded}
+    <div class="flex px-4 py-5 gap-2">
+        <div class="border-l-2 border-[#2F2F2F] min-h-[fit-content]"></div>
+        <div class="flex flex-col gap-4 ml-2">
+            {#each options as option, index (option.value)}
+                <label class="flex items-center cursor-pointer">
+                    <input 
+                        type="checkbox" 
+                        bind:checked={option.selected}
+                        class="hidden"
+                        on:change={() => toggleOption(index)}
+                    />
+                    <span 
+                        class={`w-6 h-6 rounded-full inline-block 
+                            mr-2
+                            ${option.selected ? '' : 'bg-[#2F2F2F]'}`}
+                    >
+                        {#if option.selected}
+                            <img src={checkIconYellowBg} alt="Checked" class="w-full h-full" />
+                        {/if}
+                    </span>
+                    {option.label}
+                </label>
+            {/each}
+        </div>
+    </div>
+{/if}
--- src/lib/steps/Packages.svelte
+++ src/lib/steps/Packages.svelte
@@ -1,48 +1,24 @@
 <script lang="ts">
-    import { createDisclosure} from 'svelte-headlessui';
-	import Transition from 'svelte-transition';
-
     import Footer from "../ProgressStepper.svelte";
     import DialogComponent from '../InfoDialog.svelte';
-
-    import checkIconBlackBg from "../../assets/icons/check-bg-black.svg";
-    import checkIconYellowBg from "../../assets/icons/check-bg-yellow.svg";
-    import dashIconBlackBg from "../../assets/icons/dash-bg-black.svg";
-    import dashIconYellowBg from "../../assets/icons/dash-bg-yellow.svg";
+    import PackageButton from "../PackageButton.svelte";
 
     import packagesIcon from "../../assets/icons/packages-icon.svg";
-    import arrowDownIcon from "../../assets/icons/arrow-down-black.svg";
 
-    const refund = createDisclosure({ label: 'Refund Policy', expanded: false })
-	const support = createDisclosure({ label: 'Technical Support', expanded: false })
-
-    let isHovered = false;
-    let isFocused = false;
-
-    let options = [
+    let gpuOptions = [
         { label: 'Option 1', value: 'option1' },
         { label: 'Option 2', value: 'option2' },
         { label: 'Option 3', value: 'option3' }
     ];
 
-    let selectedOptions = new Set();
+    let otherOptions = [
+        { label: 'Option 1', value: 'option1' },
+        { label: 'Option 2', value: 'option2' },
+        { label: 'Option 3', value: 'option3' }
+    ];
 
-    function toggleSelection(value) {
-        if (selectedOptions.has(value)) {
-            selectedOptions.delete(value);
-        } else {
-            selectedOptions.add(value);
-        }
-
-        selectedOptions = new Set([...selectedOptions]);
-    }
-
-
-    let allSelected = false; // New reactive variable to observe if all options are selected
-
-    $: {
-        allSelected = options.every(option => selectedOptions.has(option.value));
-    }
+    let refundExpanded = false;
+    let supportExpanded = false;
 
     export let switchView: (viewName: string) => void;
 
@@ -59,107 +35,8 @@
             </div>
             <div class="h-0.5 bg-[#2F2F2F] w-auto mt-3 mb-3"></div>
             <div class="w-full">
-                <button
-                use:refund.button
-                class="flex flex-row items-center gap-2 w-full rounded-full px-2 py-2 text-left text-sm font-medium 
-                    {($refund.expanded || isHovered || isFocused) ? 'bg-secondary-container text-black' : 'text-white'}"
-                on:mouseover={() => isHovered = true}
-                on:mouseout={() => isHovered = false}
-                on:focus={() => isFocused = true}
-                on:blur={() => isFocused = false}
-                >
-                    <img 
-                        src={
-                            allSelected 
-                                ? ($refund.expanded || isHovered || isFocused ? checkIconBlackBg : checkIconYellowBg )
-                                : ($refund.expanded || isHovered || isFocused ? dashIconBlackBg : dashIconYellowBg )
-                        } 
-                        alt="status" class="w-7" 
-                    >
-                    <span class="font-medium text-sm">GPU utilities</span>
-                    {#if $refund.expanded || isHovered || isFocused}
-                        <img src={arrowDownIcon} alt="arrow down" class="w-7 ml-auto {$refund.expanded ? 'rotate-180 transform' : ''}">
-                    {/if}
-                </button>
-
-                {#if $refund.expanded}
-                    <div class="flex px-4 py-5 gap-2">
-                        <div class="border-l-2 border-[#2F2F2F] min-h-[fit-content]"></div>
-                        <div class="flex flex-col gap-4 ml-2">
-                            {#each options as option (option.value)}
-                                <label class="flex items-center cursor-pointer">
-                                    <input 
-                                        type="checkbox" 
-                                        value={option.value}
-                                        class="hidden"
-                                        on:change={() => toggleSelection(option.value)}
-                                    />
-                                    <span 
-                                        class={`w-6 h-6 rounded-full inline-block 
-                                            mr-2
-                                            ${selectedOptions.has(option.value) ? '' : 'bg-[#2F2F2F]'}`}
-                                    >
-                                        {#if selectedOptions.has(option.value)}
-                                            <img src={checkIconYellowBg} alt="Checked" class="w-full h-full" />
-                                        {/if}
-                                    </span>
-                                    {option.label}
-                                </label>
-                            {/each}
-                        </div>
-                    </div>
-                {/if}    
-                  
-                <button
-                use:support.button
-                class="flex flex-row items-center gap-2 w-full rounded-full px-2 py-2 text-left text-sm font-medium 
-                    {($support.expanded || isHovered || isFocused) ? 'bg-secondary-container text-black' : 'text-white'}"
-                on:mouseover={() => isHovered = true}
-                on:mouseout={() => isHovered = false}
-                on:focus={() => isFocused = true}
-                on:blur={() => isFocused = false}
-                >
-                    <img 
-                        src={
-                            allSelected 
-                                ? ($support.expanded || isHovered || isFocused ? checkIconBlackBg : checkIconYellowBg )
-                                : ($support.expanded || isHovered || isFocused ? dashIconBlackBg : dashIconYellowBg )
-                        } 
-                        alt="status" class="w-7" 
-                    >
-                    <span class="font-medium text-sm">GPU utilities</span>
-                    {#if $support.expanded || isHovered || isFocused}
-                        <img src={arrowDownIcon} alt="arrow down" class="w-7 ml-auto {$support.expanded ? 'rotate-180 transform' : ''}">
-                    {/if}
-                </button>
-
-                {#if $support.expanded}
-                    <div class="flex px-4 py-5 gap-2">
-                        <div class="border-l-2 border-[#2F2F2F] min-h-[fit-content]"></div>
-                        <div class="flex flex-col gap-4 ml-2">
-                            {#each options as option (option.value)}
-                                <label class="flex items-center cursor-pointer">
-                                    <input 
-                                        type="checkbox" 
-                                        value={option.value}
-                                        class="hidden"
-                                        on:change={() => toggleSelection(option.value)}
-                                    />
-                                    <span 
-                                        class={`w-6 h-6 rounded-full inline-block 
-                                            mr-2
-                                            ${selectedOptions.has(option.value) ? '' : 'bg-[#2F2F2F]'}`}
-                                    >
-                                        {#if selectedOptions.has(option.value)}
-                                            <img src={checkIconYellowBg} alt="Checked" class="w-full h-full" />
-                                        {/if}
-                                    </span>
-                                    {option.label}
-                                </label>
-                            {/each}
-                        </div>
-                    </div>
-                {/if}    
+                <PackageButton label="GPU utilities" options={gpuOptions} bind:expanded={refundExpanded} />
+                <PackageButton label="Technical Support" options={otherOptions} bind:expanded={supportExpanded} />
             </div>
         </div>
         <div class="w-0.5 h-full bg-[#2F2F2F]"></div>
