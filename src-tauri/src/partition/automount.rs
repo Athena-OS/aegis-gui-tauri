86476f8912ddca76bd0559a93c66751d89f7b041
root <root@Wakati>
2024-02-18 21:31:15 +0300

    automount

---
+++ src-tauri/src/partition/automount.rs
@@ -0,0 +1,59 @@
+use dbus::{
+    blocking::{Connection, BlockingSender},
+    message::Message
+};
+use std::{
+    time::Duration,
+    string::String
+};
+
+pub struct AutoMountInfo {
+    pub has_solid: bool,
+    pub was_solid_module_auto_loaded: bool,
+}
+
+pub fn kded_call(method: &str, args: Vec<&dyn dbus::arg::RefArg>) -> Message {
+    Message::new_method_call(
+        "org.kde.kded5",
+        "/kded",
+        "org.kde.kded5",
+        method,
+    ).unwrap().append_ref(&args)
+}
+
+pub fn enable_solid_automount(dbus: &Connection, enable: bool) {
+    let method = if enable { "loadModule" } else { "unloadModule" };
+    let arg = String::from("device_automounter");
+    let args = vec![&arg as &dyn dbus::arg::RefArg];
+    let msg = kded_call(method, args);
+    let reply = dbus.send_with_reply_and_block(msg, Duration::from_millis(2000));
+
+    match reply {
+        Ok(r) => println!("Reply: {:?}", r.get_items()),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+pub fn query_solid_automount(dbus: &Connection) -> AutoMountInfo {
+    let arg = String::from("device_automounter");
+    let args = vec![&arg as &dyn dbus::arg::RefArg];
+    let msg = kded_call("isModuleAutoloaded", args);
+    let reply = dbus.send_with_reply_and_block(msg, Duration::from_millis(2000));
+
+    match reply {
+        Ok(r) => {
+            let result: bool = r.get1().unwrap_or(false);
+            AutoMountInfo {
+                has_solid: true,
+                was_solid_module_auto_loaded: result,
+            }
+        },
+        Err(e) => {
+            println!("Error querying Solid: {}", e);
+            AutoMountInfo {
+                has_solid: false,
+                was_solid_module_auto_loaded: false,
+            }
+        },
+    }
+}
